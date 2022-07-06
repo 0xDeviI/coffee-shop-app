3f78b9d61e51a5748fe1183800cf73bf5ef67b7b
0xDeviI <arminloveslinux@gmail.com>
2022-07-07 02:28:22 +0430

    jwt secret key moved to configs | unstable :hammer: :rotating_light:

--- server/config.py
+++ server/config.py
@@ -4,3 +4,7 @@ MYSQL_USER = 'root'
 MYSQL_PASSWORD = ''
 MYSQL_DB = 'coffeedb'
 #------------------------------#
+# //////////////////////////// #
+#--------#  Security  #--------#
+JWT_SECRET_KEY = """/RhCT6gsI9URVoGLkHF6TNGMFFIFrmQUvU9WMbpcBjRIVGsMXZMEXSsrCQgSjAPS7T5CQufZ7/59apa77bi2Ns0K0cvwY2mIyRrYpKX/+IjDVx2siTRPL28ZlAOrGOWDCBh3KVBau185+Yo+GztA/FmpQ6DYdLGS2Sp5mU2Eljm8sZa2/eB/ONCJnMsB5mjhiU/UIIj3haUGGprGLOwyctLe2qajWrAPR7kt7KCMYVtpfF+u5v5oQoWiHKqrg5VqUaJMP9lq+fefJlQRuak8ptvjWHvvhl1wH5Hj4rvnTg9pSJT1aGdeafobEqxrRkmVg10CdxMptO53ztj5uteUFQ=="""
+#------------------------------#
--- server/server.py
+++ server/server.py
@@ -21,7 +21,7 @@ app.config['MYSQL_PASSWORD'] = MYSQL_PASSWORD
 app.config['MYSQL_DB'] = MYSQL_DB
 
 mySql = MySQL(app)
-app.config["JWT_SECRET_KEY"] = """/RhCT6gsI9URVoGLkHF6TNGMFFIFrmQUvU9WMbpcBjRIVGsMXZMEXSsrCQgSjAPS7T5CQufZ7/59apa77bi2Ns0K0cvwY2mIyRrYpKX/+IjDVx2siTRPL28ZlAOrGOWDCBh3KVBau185+Yo+GztA/FmpQ6DYdLGS2Sp5mU2Eljm8sZa2/eB/ONCJnMsB5mjhiU/UIIj3haUGGprGLOwyctLe2qajWrAPR7kt7KCMYVtpfF+u5v5oQoWiHKqrg5VqUaJMP9lq+fefJlQRuak8ptvjWHvvhl1wH5Hj4rvnTg9pSJT1aGdeafobEqxrRkmVg10CdxMptO53ztj5uteUFQ=="""
+app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
 jwt = JWTManager(app)
 
 @app.route('/')
